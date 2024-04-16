8b7dd02d0691ec3998238fde40ad4fc662bedd6c
nuzhattanjil <104226976+nuzhattanjil@users.noreply.github.com>
2024-04-15 22:59:42 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -8,4 +8,14 @@ window.addEventListener("load", function() {
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("player1");
+    video.autoplay = false;
+    video.loop = false;
+});
 
+document.getElementById("#play").addEventListener("click", function() {
+    video.play();
+    var volumeSpan = document.getElementById("volume");
+    volumeSpan.textContent = video.volume * 100 + "%";
+});
